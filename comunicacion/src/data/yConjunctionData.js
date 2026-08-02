fb8aadda23e35fda4bab7a0a4054a78f1b61f5ab
Ninaswiftie09 <naj231088@uvg.edu.gt>
2026-08-01 19:16:37 -0600

    feat: add book-based content for conjunction y lesson

---
+++ comunicacion/src/data/yConjunctionData.js
@@ -0,0 +1,234 @@
+export const yReading = {
+  title: 'El mago y su varita mágica',
+  introduction:
+    'En esta historia conoceremos una palabra que nos ayuda a unir personas, animales y objetos.',
+  paragraphs: [
+    'Un mago llegó a la escuela con un sombrero y una varita mágica.',
+    'Del sombrero sacó una paloma y un pañuelo.',
+    'Después mostró un conejo y una cuchara.',
+    'Todos observaron felices los objetos que aparecían.',
+    'El mago explicó que la palabra y sirve para unir dos elementos.',
+  ],
+  image: '/images/lecciones/y/mago-varita.png',
+  imageAlt:
+    'Un mago presenta objetos frente a varios niños en la escuela',
+  instructionAudio:
+    '/audio/lecciones/y/instruccion-lectura.mp3',
+  readingAudio:
+    '/audio/lecciones/y/el-mago-y-su-varita.mp3',
+}
+
+export const page26Pairs = [
+  {
+    id: 'pelota-tambor',
+    first: {
+      id: 'pelota',
+      name: 'pelota',
+      image: '/images/lecciones/y/pelota.png',
+      audio: '/audio/lecciones/y/pelota.mp3',
+    },
+    second: {
+      id: 'tambor',
+      name: 'tambor',
+      image: '/images/lecciones/y/tambor.png',
+      audio: '/audio/lecciones/y/tambor.mp3',
+    },
+    phraseAudio:
+      '/audio/lecciones/y/pelota-y-tambor.mp3',
+  },
+  {
+    id: 'manzana-banano',
+    first: {
+      id: 'manzana',
+      name: 'manzana',
+      image: '/images/lecciones/y/manzana.png',
+      audio: '/audio/lecciones/y/manzana.mp3',
+    },
+    second: {
+      id: 'banano',
+      name: 'banano',
+      image: '/images/lecciones/y/banano.png',
+      audio: '/audio/lecciones/y/banano.mp3',
+    },
+    phraseAudio:
+      '/audio/lecciones/y/manzana-y-banano.mp3',
+  },
+  {
+    id: 'camisa-pantalon',
+    first: {
+      id: 'camisa',
+      name: 'camisa',
+      image: '/images/lecciones/y/camisa.png',
+      audio: '/audio/lecciones/y/camisa.mp3',
+    },
+    second: {
+      id: 'pantalon',
+      name: 'pantalón',
+      image: '/images/lecciones/y/pantalon.png',
+      audio: '/audio/lecciones/y/pantalon.mp3',
+    },
+    phraseAudio:
+      '/audio/lecciones/y/camisa-y-pantalon.mp3',
+  },
+  {
+    id: 'cuaderno-lapiz',
+    first: {
+      id: 'cuaderno',
+      name: 'cuaderno',
+      image: '/images/lecciones/y/cuaderno.png',
+      audio: '/audio/lecciones/y/cuaderno.mp3',
+    },
+    second: {
+      id: 'lapiz',
+      name: 'lápiz',
+      image: '/images/lecciones/y/lapiz.png',
+      audio: '/audio/lecciones/y/lapiz.mp3',
+    },
+    phraseAudio:
+      '/audio/lecciones/y/cuaderno-y-lapiz.mp3',
+  },
+]
+
+export const page27Pairs = [
+  {
+    id: 'arcoiris-sol',
+    first: {
+      id: 'arcoiris',
+      name: 'arcoíris',
+      image: '/images/lecciones/y/arcoiris.png',
+      audio: '/audio/lecciones/y/arcoiris.mp3',
+    },
+    second: {
+      id: 'sol',
+      name: 'sol',
+      image: '/images/lecciones/y/sol.png',
+      audio: '/audio/lecciones/y/sol.mp3',
+    },
+    phraseAudio:
+      '/audio/lecciones/y/arcoiris-y-sol.mp3',
+  },
+  {
+    id: 'pez-lago',
+    first: {
+      id: 'pez',
+      name: 'pez',
+      image: '/images/lecciones/y/pez.png',
+      audio: '/audio/lecciones/y/pez.mp3',
+    },
+    second: {
+      id: 'lago',
+      name: 'lago',
+      image: '/images/lecciones/y/lago.png',
+      audio: '/audio/lecciones/y/lago.mp3',
+    },
+    phraseAudio:
+      '/audio/lecciones/y/pez-y-lago.mp3',
+  },
+  {
+    id: 'taza-panes',
+    first: {
+      id: 'taza',
+      name: 'taza',
+      image: '/images/lecciones/y/taza.png',
+      audio: '/audio/lecciones/y/taza.mp3',
+    },
+    second: {
+      id: 'panes',
+      name: 'panes',
+      image: '/images/lecciones/y/panes.png',
+      audio: '/audio/lecciones/y/panes.mp3',
+    },
+    phraseAudio:
+      '/audio/lecciones/y/taza-y-panes.mp3',
+  },
+]
+
+export const finalExercises = [
+  {
+    id: 'mama-susi',
+    instruction:
+      'Selecciona la imagen que completa la frase.',
+    first: {
+      name: 'Mamá',
+      image: '/images/lecciones/y/mama.png',
+      imageAlt: 'Una mamá',
+    },
+    answer: 'Susi',
+    options: [
+      {
+        name: 'Susi',
+        image: '/images/lecciones/y/susi.png',
+      },
+      {
+        name: 'sol',
+        image: '/images/lecciones/y/sol.png',
+      },
+      {
+        name: 'panes',
+        image: '/images/lecciones/y/panes.png',
+      },
+    ],
+    exerciseAudio:
+      '/audio/lecciones/y/ejercicio-mama-y-susi.mp3',
+    phraseAudio:
+      '/audio/lecciones/y/mama-y-susi.mp3',
+  },
+  {
+    id: 'limon-mar',
+    instruction:
+      'Selecciona la imagen que completa la frase.',
+    first: {
+      name: 'limón',
+      image: '/images/lecciones/y/limon.png',
+      imageAlt: 'Un limón',
+    },
+    answer: 'mar',
+    options: [
+      {
+        name: 'lápiz',
+        image: '/images/lecciones/y/lapiz.png',
+      },
+      {
+        name: 'mar',
+        image: '/images/lecciones/y/mar.png',
+      },
+      {
+        name: 'tambor',
+        image: '/images/lecciones/y/tambor.png',
+      },
+    ],
+    exerciseAudio:
+      '/audio/lecciones/y/ejercicio-limon-y-mar.mp3',
+    phraseAudio:
+      '/audio/lecciones/y/limon-y-mar.mp3',
+  },
+  {
+    id: 'mesa-sal',
+    instruction:
+      'Selecciona la imagen que completa la frase.',
+    first: {
+      name: 'mesa',
+      image: '/images/lecciones/y/mesa.png',
+      imageAlt: 'Una mesa',
+    },
+    answer: 'sal',
+    options: [
+      {
+        name: 'sal',
+        image: '/images/lecciones/y/sal.png',
+      },
+      {
+        name: 'banano',
+        image: '/images/lecciones/y/banano.png',
+      },
+      {
+        name: 'camisa',
+        image: '/images/lecciones/y/camisa.png',
+      },
+    ],
+    exerciseAudio:
+      '/audio/lecciones/y/ejercicio-mesa-y-sal.mp3',
+    phraseAudio:
+      '/audio/lecciones/y/mesa-y-sal.mp3',
+  },
+]
