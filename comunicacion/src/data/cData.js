f70c18f07ca916c0b14b53628b765c5e7d09f930
Ninaswiftie09 <naj231088@uvg.edu.gt>
2026-08-12 23:58:57 -0600

    feat: add letter C lesson content data

---
+++ comunicacion/src/data/cData.js
@@ -0,0 +1,483 @@
+export const cReading = {
+  title: 'El caldo de coco',
+  paragraphs: [
+    'Paco es un cocinero muy bueno. Él cocina en un comedor del mercado, muy famoso por su caldo de coco. Por eso, ese comedor casi siempre está lleno de gente.',
+    'Cuando el comedor está cerrado, Paco aprovecha para cocinar el delicioso caldo de coco. Primero, sale a comprar todo lo que necesitará. Después, empieza a cocinar. A veces aparta un poco de coco, para prepararse unas ricas cocadas. A Paco le gusta cocinar y cuando lo hace siempre canta, y canta así:',
+    '¡Me encanta el coco! ¡Cómo como coco!',
+    'Cuando como coco, cocadas de coco como.',
+    'Como coco y tomo caldo de coco. ¡Me encanta el coco!',
+  ],
+  image: '/images/lecciones/c/lectura-caldo-coco.png',
+  imageAlt: 'Paco cocinando caldo de coco en un comedor del mercado',
+  instructionAudio: '/audio/lecciones/c/instruccion-lectura.mp3',
+  readingAudio: '/audio/lecciones/c/caldo-de-coco.mp3',
+}
+
+export const cComprehensionQuestions = [
+  '¿Conoces el coco? ¿Cómo es?',
+  '¿Qué cocina Paco con el coco?',
+  '¿Por qué crees que Paco canta cuando cocina?',
+  '¿Qué dice la canción de Paco?',
+]
+
+export const cSoundIntro = {
+  word: 'coco',
+  image: '/images/lecciones/c/coco.png',
+  imageAlt: 'Un coco abierto con pajilla',
+  wordAudio: '/audio/lecciones/c/coco.mp3',
+  soundAudio: '/audio/lecciones/c/sonido-c.mp3',
+  instructionAudio: '/audio/lecciones/c/instruccion-sonido-c.mp3',
+}
+
+export const cSoundExamples = [
+  {
+    word: 'casa',
+    audio: '/audio/lecciones/c/casa.mp3',
+  },
+  {
+    word: 'come',
+    audio: '/audio/lecciones/c/come.mp3',
+  },
+  {
+    word: 'cura',
+    audio: '/audio/lecciones/c/cura.mp3',
+  },
+]
+
+export const cInitialSoundInstructionAudio =
+  '/audio/lecciones/c/instruccion-seleccion-inicial-c.mp3'
+
+export const cInitialSoundImages = [
+  {
+    id: 'caballo',
+    name: 'caballo',
+    image: '/images/lecciones/c/caballo.png',
+    audio: '/audio/lecciones/c/caballo.mp3',
+    startsWithC: true,
+  },
+  {
+    id: 'corazon',
+    name: 'corazón',
+    image: '/images/lecciones/c/corazon.png',
+    audio: '/audio/lecciones/c/corazon.mp3',
+    startsWithC: true,
+  },
+  {
+    id: 'margarita',
+    name: 'margarita',
+    image: '/images/lecciones/c/margarita.png',
+    audio: '/audio/lecciones/c/margarita.mp3',
+    startsWithC: false,
+  },
+  {
+    id: 'cuna',
+    name: 'cuna',
+    image: '/images/lecciones/c/cuna.png',
+    audio: '/audio/lecciones/c/cuna.mp3',
+    startsWithC: true,
+  },
+]
+
+export const cMiddleSoundInstructionAudio =
+  '/audio/lecciones/c/instruccion-sonido-segunda-silaba-c.mp3'
+
+export const cMiddleSoundWords = [
+  {
+    id: 'banca',
+    word: 'banca',
+    syllables: ['ban', 'ca'],
+    answerIndex: 1,
+    audio: '/audio/lecciones/c/banca.mp3',
+  },
+  {
+    id: 'pico',
+    word: 'pico',
+    syllables: ['pi', 'co'],
+    answerIndex: 1,
+    audio: '/audio/lecciones/c/pico.mp3',
+  },
+  {
+    id: 'taco',
+    word: 'taco',
+    syllables: ['ta', 'co'],
+    answerIndex: 1,
+    audio: '/audio/lecciones/c/taco.mp3',
+  },
+  {
+    id: 'blanca',
+    word: 'blanca',
+    syllables: ['blan', 'ca'],
+    answerIndex: 1,
+    audio: '/audio/lecciones/c/blanca.mp3',
+  },
+]
+
+export const cRhymeInstructionAudio =
+  '/audio/lecciones/c/instruccion-rimas-c.mp3'
+
+export const cRhymeExercises = [
+  {
+    id: 'foco',
+    word: 'foco',
+    image: '/images/lecciones/c/foco.png',
+    audio: '/audio/lecciones/c/foco.mp3',
+    answer: 'coco',
+    options: [
+      {
+        name: 'coco',
+        image: '/images/lecciones/c/coco.png',
+        audio: '/audio/lecciones/c/coco.mp3',
+      },
+      {
+        name: 'arco',
+        image: '/images/lecciones/c/arco.png',
+        audio: '/audio/lecciones/c/arco.mp3',
+      },
+      {
+        name: 'vaca',
+        image: '/images/lecciones/c/vaca.png',
+        audio: '/audio/lecciones/c/vaca.mp3',
+      },
+    ],
+  },
+  {
+    id: 'barco',
+    word: 'barco',
+    image: '/images/lecciones/c/barco.png',
+    audio: '/audio/lecciones/c/barco.mp3',
+    answer: 'arco',
+    options: [
+      {
+        name: 'vaca',
+        image: '/images/lecciones/c/vaca.png',
+        audio: '/audio/lecciones/c/vaca.mp3',
+      },
+      {
+        name: 'arco',
+        image: '/images/lecciones/c/arco.png',
+        audio: '/audio/lecciones/c/arco.mp3',
+      },
+      {
+        name: 'coco',
+        image: '/images/lecciones/c/coco.png',
+        audio: '/audio/lecciones/c/coco.mp3',
+      },
+    ],
+  },
+  {
+    id: 'hamaca',
+    word: 'hamaca',
+    image: '/images/lecciones/c/hamaca.png',
+    audio: '/audio/lecciones/c/hamaca.mp3',
+    answer: 'vaca',
+    options: [
+      {
+        name: 'arco',
+        image: '/images/lecciones/c/arco.png',
+        audio: '/audio/lecciones/c/arco.mp3',
+      },
+      {
+        name: 'coco',
+        image: '/images/lecciones/c/coco.png',
+        audio: '/audio/lecciones/c/coco.mp3',
+      },
+      {
+        name: 'vaca',
+        image: '/images/lecciones/c/vaca.png',
+        audio: '/audio/lecciones/c/vaca.mp3',
+      },
+    ],
+  },
+]
+
+export const cSyllableInstructionAudio =
+  '/audio/lecciones/c/instruccion-silabas-c.mp3'
+
+export const cSyllableAudios = {
+  ca: '/audio/lecciones/c/silaba-ca.mp3',
+  co: '/audio/lecciones/c/silaba-co.mp3',
+  cu: '/audio/lecciones/c/silaba-cu.mp3',
+}
+
+export const cSyllableImageExercises = [
+  {
+    id: 'caja',
+    word: 'caja',
+    syllable: 'ca',
+    image: '/images/lecciones/c/caja.png',
+    audio: '/audio/lecciones/c/caja.mp3',
+  },
+  {
+    id: 'codo',
+    word: 'codo',
+    syllable: 'co',
+    image: '/images/lecciones/c/codo.png',
+    audio: '/audio/lecciones/c/codo.mp3',
+  },
+  {
+    id: 'cubo',
+    word: 'cubo',
+    syllable: 'cu',
+    image: '/images/lecciones/c/cubo.png',
+    audio: '/audio/lecciones/c/cubo.mp3',
+  },
+]
+
+export const cSyllableSearchInstructionAudio =
+  '/audio/lecciones/c/instruccion-buscar-silabas-c.mp3'
+
+export const cSyllableSearchExercises = [
+  {
+    target: 'co',
+    words: [
+      'piso',
+      'comida',
+      'capa',
+      'cono',
+      'pico',
+      'saco',
+      'casa',
+    ],
+    answers: [
+      'comida',
+      'cono',
+      'pico',
+      'saco',
+    ],
+  },
+  {
+    target: 'ca',
+    words: [
+      'malo',
+      'casa',
+      'poca',
+      'calor',
+      'cana',
+      'cama',
+      'carro',
+    ],
+    answers: [
+      'casa',
+      'poca',
+      'calor',
+      'cana',
+      'cama',
+      'carro',
+    ],
+  },
+  {
+    target: 'cu',
+    words: [
+      'camino',
+      'cura',
+      'cola',
+      'cuna',
+      'cupo',
+      'cupido',
+      'codo',
+    ],
+    answers: [
+      'cura',
+      'cuna',
+      'cupo',
+      'cupido',
+    ],
+  },
+]
+
+export const cCompletionInstructionAudio =
+  '/audio/lecciones/c/instruccion-completar-palabras-c.mp3'
+
+export const cCompletionExercises = [
+  {
+    id: 'carro',
+    image: '/images/lecciones/c/carro.png',
+    imageAlt: 'Un carro rojo',
+    prefix: '',
+    suffix: 'rro',
+    answer: 'ca',
+    word: 'carro',
+    audio: '/audio/lecciones/c/carro.mp3',
+  },
+  {
+    id: 'coco',
+    image: '/images/lecciones/c/coco.png',
+    imageAlt: 'Un coco abierto',
+    prefix: 'co',
+    suffix: '',
+    answer: 'co',
+    word: 'coco',
+    audio: '/audio/lecciones/c/coco.mp3',
+  },
+  {
+    id: 'cuna',
+    image: '/images/lecciones/c/cuna.png',
+    imageAlt: 'Una cuna',
+    prefix: '',
+    suffix: 'na',
+    answer: 'cu',
+    word: 'cuna',
+    audio: '/audio/lecciones/c/cuna.mp3',
+  },
+  {
+    id: 'copa',
+    image: '/images/lecciones/c/copa.png',
+    imageAlt: 'Una copa',
+    prefix: '',
+    suffix: 'pa',
+    answer: 'co',
+    word: 'copa',
+    audio: '/audio/lecciones/c/copa.mp3',
+  },
+]
+
+export const cJoinInstructionAudio =
+  '/audio/lecciones/c/instruccion-unir-silabas-c.mp3'
+
+export const cJoinExercises = [
+  {
+    id: 'copa',
+    image: '/images/lecciones/c/copa.png',
+    imageAlt: 'Una copa',
+    firstSyllable: 'co',
+    answer: 'pa',
+    options: ['co', 'pa', 'sa'],
+    word: 'copa',
+    audio: '/audio/lecciones/c/copa.mp3',
+  },
+  {
+    id: 'taco',
+    image: '/images/lecciones/c/taco.png',
+    imageAlt: 'Un taco en un plato',
+    firstSyllable: 'ta',
+    answer: 'co',
+    options: ['pa', 'co', 'sa'],
+    word: 'taco',
+    audio: '/audio/lecciones/c/taco.mp3',
+  },
+  {
+    id: 'casa',
+    image: '/images/lecciones/c/casa.png',
+    imageAlt: 'Una casa',
+    firstSyllable: 'ca',
+    answer: 'sa',
+    options: ['co', 'pa', 'sa'],
+    word: 'casa',
+    audio: '/audio/lecciones/c/casa.mp3',
+  },
+  {
+    id: 'saco',
+    image: '/images/lecciones/c/saco.png',
+    imageAlt: 'Un saco',
+    firstSyllable: 'sa',
+    answer: 'co',
+    options: ['sa', 'pa', 'co'],
+    word: 'saco',
+    audio: '/audio/lecciones/c/saco.mp3',
+  },
+]
+
+export const cFinalWordsInstructionAudio =
+  '/audio/lecciones/c/instruccion-palabras-finales-c.mp3'
+
+export const cFinalWords = [
+  {
+    word: 'Carlos',
+    image: '/images/lecciones/c/carlos.png',
+    audio: '/audio/lecciones/c/carlos.mp3',
+  },
+  {
+    word: 'cama',
+    image: '/images/lecciones/c/cama.png',
+    audio: '/audio/lecciones/c/cama.mp3',
+  },
+  {
+    word: 'cara',
+    image: '/images/lecciones/c/cara.png',
+    audio: '/audio/lecciones/c/cara.mp3',
+  },
+  {
+    word: 'roca',
+    image: '/images/lecciones/c/roca.png',
+    audio: '/audio/lecciones/c/roca.mp3',
+  },
+  {
+    word: 'cola',
+    image: '/images/lecciones/c/cola.png',
+    audio: '/audio/lecciones/c/cola.mp3',
+  },
+  {
+    word: 'corona',
+    image: '/images/lecciones/c/corona.png',
+    audio: '/audio/lecciones/c/corona.mp3',
+  },
+]
+
+export const cSentenceInstructionAudio =
+  '/audio/lecciones/c/instruccion-oraciones-c.mp3'
+
+export const cSentenceExercises = [
+  {
+    id: 'paco-taco',
+    sentence: 'Paco come su ___.',
+    answer: 'taco',
+    sentenceAudio:
+      '/audio/lecciones/c/oracion-paco-come.mp3',
+    completedAudio:
+      '/audio/lecciones/c/paco-come-su-taco.mp3',
+    options: [
+      {
+        name: 'taco',
+        image: '/images/lecciones/c/taco.png',
+        audio: '/audio/lecciones/c/taco.mp3',
+      },
+      {
+        name: 'camisa',
+        image: '/images/lecciones/c/camisa.png',
+        audio: '/audio/lecciones/c/camisa.mp3',
+      },
+    ],
+  },
+  {
+    id: 'carol-comal',
+    sentence: 'Carol usa mi ___.',
+    answer: 'comal',
+    sentenceAudio:
+      '/audio/lecciones/c/oracion-carol-usa.mp3',
+    completedAudio:
+      '/audio/lecciones/c/carol-usa-mi-comal.mp3',
+    options: [
+      {
+        name: 'sandía',
+        image: '/images/lecciones/c/sandia.png',
+        audio: '/audio/lecciones/c/sandia.mp3',
+      },
+      {
+        name: 'comal',
+        image: '/images/lecciones/c/comal.png',
+        audio: '/audio/lecciones/c/comal.mp3',
+      },
+    ],
+  },
+  {
+    id: 'mula-cola',
+    sentence: 'La mula lame su ___.',
+    answer: 'cola',
+    sentenceAudio:
+      '/audio/lecciones/c/oracion-mula-lame.mp3',
+    completedAudio:
+      '/audio/lecciones/c/la-mula-lame-su-cola.mp3',
+    options: [
+      {
+        name: 'cola',
+        image: '/images/lecciones/c/cola.png',
+        audio: '/audio/lecciones/c/cola.mp3',
+      },
+      {
+        name: 'perro',
+        image: '/images/lecciones/c/perro.png',
+        audio: '/audio/lecciones/c/perro.mp3',
+      },
+    ],
+  },
+]
